e8518eac168d6386cd04fb038528a238a88f5124
saitejteju <saitej7901@gmail.com>
2024-04-29 10:49:13 +0530

    python

---
+++ square_root.py
@@ -0,0 +1,9 @@
+# solution 1
+# num = int(input('enter a number: '))
+# sr = num**(0.5)
+# print("the square root of the given number is ", num)
+
+import math
+num = int(input("enter a value : "))
+sr = math.sqrt(num)
+print("the square root of given num is ", sr)
